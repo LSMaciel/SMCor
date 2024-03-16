12206f60a1ef256c382b6fbafc2f906b4906b6d9
Lucas Maciel <lucasmaciel.hc@gmail.com>
2024-03-16 16:26:41 -0300

    animação parceiros

--- JS/script.js
+++ JS/script.js
@@ -95,3 +95,26 @@ class FormSubmit {
     error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
   });
   formSubmit.init();
+
+
+  //scroll parceiros
+
+  const scrollers = document.querySelectorAll('.scroller')
+  if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+      addAnimation();
+  }
+  function addAnimation(){
+      scrollers.forEach((scroller)=> {
+          scroller.setAttribute("data-animated", true);
+
+          const scrollerInner = scroller.querySelector('.scroller__inner');
+          const scrollerContent = Array.from(scrollerInner.children);
+
+          scrollerContent.forEach(item => {
+              const duplicatedItem = item.cloneNode(true);
+              duplicatedItem.setAttribute("aria-hidden",true);
+              scrollerInner.appendChild(duplicatedItem)
+              
+          })
+      })
+  }
